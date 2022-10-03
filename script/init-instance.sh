57f29a0c018fe6e1ce063a3cd28032fe04232c30
rollrat <rollrat.cse@gmail.com>
2022-10-03 17:45:22 +0900

    script: add init-instance script

---
+++ script/init-instance.sh
@@ -0,0 +1,10 @@
+# install docker
+sudo yum update
+sudo yum install docker
+
+# docker service start
+sudo service dockerd start
+
+# install scs-http-packet-divert
+# install cAdvisor
+# install jaeger
