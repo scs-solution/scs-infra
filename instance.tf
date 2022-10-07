6cfc00670d98f9efe213e459811ef0db0dc4bd11
rollrat <rollrat.cse@gmail.com>
2022-10-07 13:58:17 +0900

    spot instance test

--- instance.tf
+++ instance.tf
@@ -20,12 +20,29 @@ data "aws_security_group" "default" {
 }
 
 # https://docs.aws.amazon.com/AL2/latest/relnotes/relnotes-al2.html
-resource "aws_instance" "web" {
+# resource "aws_instance" "web" {
+#   ami           = "ami-0c76973fbe0ee100c" # amazon/amzn2-ami-kernel-5.10-hvm-2.0.20220912.1-x86_64-gp2
+#   instance_type = "t2.micro"
+#   key_name      = aws_key_pair.web_admin.key_name
+#   vpc_security_group_ids = [
+#     aws_security_group.ssh.id,
+#     data.aws_security_group.default.id
+#   ]
+# }
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/spot_instance_request
+resource "aws_spot_instance_request" "cheap_worker" {
   ami           = "ami-0c76973fbe0ee100c" # amazon/amzn2-ami-kernel-5.10-hvm-2.0.20220912.1-x86_64-gp2
   instance_type = "t2.micro"
+  spot_price    = "0.03"
+
   key_name      = aws_key_pair.web_admin.key_name
   vpc_security_group_ids = [
     aws_security_group.ssh.id,
     data.aws_security_group.default.id
   ]
+
+  tags = {
+    Name = "CheapWorker"
+  }
 }
